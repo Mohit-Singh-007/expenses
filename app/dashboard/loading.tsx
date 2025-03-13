a11aa193a981544702d0ff4f8348cf9fbf4d6b02
Mohit <msms789549@gmail.com>
2025-03-13 13:19:19 +0530

    loading-ui-02

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -1,5 +1,5 @@
 export default function Loading() {
   return (
-    <div className="w-full h-screen bg-gray-200 animate-pulse rounded-lg mt-10" />
+    <div className="w-full h-screen bg-gray-100 animate-pulse rounded-lg mt-10" />
   );
 }
